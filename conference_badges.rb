1ced38a49224b17605ae61df6a004888bf1d1436
David Chiu <davidwchiu@gmail.com>
2018-03-22 01:08:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+"Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  returnArr = []
+  arr.each do |name|
+    returnArr.push(badge_maker(name))
+  end
+  returnArr
+end
+
+def assign_rooms(list)
+  rooms = []
+    list.each_with_index do |speaker, room|
+    rooms.push("Hello, #{speaker}! You'll be assigned to room #{room+1}!")
+end
+  rooms
+end
